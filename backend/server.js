ebcf5fc71f311a68d64ffc51e7ff9a9ef0c64557
Lovanirina R <lovanirina.ran@gmail.com>
2022-11-20 14:35:24 +0300

    add dotenv to projet

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.set('port', process.env.PORT || 3000);
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, function() {
-    console.log('Listen on port : http://localhost:3000')
+server.listen(process.env.PORT || 3000, process.env.HOST || '127.0.0.1', function() {
+    console.log(`Server started listen on : http://${process.env.HOST || '127.0.0.1'}:${server.address().port}`)
 });
